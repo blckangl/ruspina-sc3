113f88729d8396e334ae260ab79afa1731925c84
Diakahril <dhia.kahri@lobsterware.com>
2022-01-24 20:41:52 +0100

    custom button directive

--- src/app/custom-button.directive.ts
+++ src/app/custom-button.directive.ts
@@ -1,10 +1,36 @@
-import { Directive } from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, Input, OnInit} from '@angular/core';
 
 @Directive({
   selector: '[appCustomButton]'
 })
-export class CustomButtonDirective {
+export class CustomButtonDirective implements OnInit {
 
-  constructor() { }
+  @Input() customBgColor: string = '#d32f2f';
+  @Input() defaultBgColor: string = 'transparent';
+
+  @HostBinding('style.backgroundColor') bgColor!: string;
+  @HostBinding('style.color') color!: string;
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.bgColor = this.customBgColor;
+    this.color = '#fff';
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.bgColor = this.defaultBgColor;
+    this.color = this.customBgColor;
+  }
+
+  constructor(private el: ElementRef) {
+
+    this.el.nativeElement.style.cursor='pointer';
+    el.nativeElement.style.borderRadius = '6px'
+  }
+
+  ngOnInit(): void {
+    this.el.nativeElement.style.border = `1px solid ${this.customBgColor}`
+    this.color = this.customBgColor;
+    this.bgColor = this.defaultBgColor;
+  }
 
 }
